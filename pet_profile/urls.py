e5a6a9f1e35e17c7ce4efd61eb61e8dcbe47cab5
Michael Starr <starrtennis@gmail.com>
2023-02-22 22:07:20 -0800

    .venv and significant model & view improvements

--- pet_profile/urls.py
+++ pet_profile/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from pet_profile import views
 
 urlpatterns = [
-    path("pet/<slug>/", views.PetDetailView.as_view(), name = "pet_details"),
+    path("pet/<slug:slug>/", views.PetDetailView.as_view(), name = "pet_profile"),
+    path("owner/<slug:slug>/",cd views.PetOwnerProfileView.as_view(), name = "owner_profile"),
 ]
