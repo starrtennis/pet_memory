a2bb83dbd03be5b1b88c9b8532382365151df746
mostarrcoding <michaelstarr.coding@gmail.com>
2023-07-14 17:18:33 -0700

    reinitializing github repo

--- pet_profile/forms.py
+++ pet_profile/forms.py
@@ -7,11 +7,18 @@ class StoryUploadForm(forms.Form):
     pet_story = forms.CharField()
 
 class AccountCreationForm(forms.Form):
+    CONTACT_METHOD_CHOICES = (
+        ("Phone", "Phone"),
+        ("Email", "Email"),
+        ("Snailmail", "Snailmail"),
+        ("Find me", "Find me"),
+    )
     account_name = forms.CharField()
     account_owner = forms.CharField()
     account_destruction_date = forms.DateField()  #should the destruction date be a date field?
     physical_user_location = forms.ChoiceField()
-    owner_contact_method = forms.CharField()
+    owner_contact_method = forms.ChoiceFieldField(choices=CONTACT_METHOD_CHOICES)
+    owner_contact_information = forms.CharField()
     owner_age = forms.IntegerField()
 
 class UpdateForm(forms.Form):
