58934081ecc9316ab958e0b668c4c9a0e87f71cb
mkstarr <starrtennis@gmail.com>
2023-10-03 06:38:38 -0700

    images not displaying again

--- pet_memorial/settings.py
+++ pet_memorial/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 
--- pet_memorial/views.py
+++ pet_memorial/views.py
@@ -3,10 +3,10 @@ from pet_profile.models import PetOwner, PetPhoto
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context['pet_owners'] = PetOwner.objects.all()
+        context['pet_photos'] = PetPhoto.objects.all()
         return context
-        
-        #use context processor class? metaclass?
-
+    
