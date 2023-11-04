be74dab8fc7f6b794a1e9a3a17a495cbd28ec755
mkstarr <starrtennis@gmail.com>
2023-11-03 17:03:26 -0700

    minor cleanup and review

--- pet_profile/urls.py
+++ pet_profile/urls.py
@@ -7,10 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path("pet/photoupload/", views.PetPhotoUploadView.as_view(), name = "photo_upload"),
     path("owner/<slug:slug>/", views.PetOwnerDetailView.as_view(), name = "owner_profile"), #slug is ambiguous, be more specific
-    path("pet/<slug:slug>/", views.PetDetailView.as_view(), name = "pet_profile"), #same as above (ibid.)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #media does show--good!
-
-
-    #I believe the photo upload page does not need to be unique to each pet, as
-    #you can just retrieve the pet's id or somesuch on the spot (puns uninteneded) to specify
-    #where the photo belongs.
+    path("pet/<slug:slug>/", views.PetDetailView.as_view(), name = "pet_profile"), #same as above
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #media doesn't show--good!
