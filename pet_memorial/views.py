911ee93b0cd07c8387ec78e17c255ee0d61d3a40
mostarrcoding <mostarrcoding@gmail.com>
2023-12-12 15:24:09 -0800

    Starting pet gallery coding

--- pet_memorial/views.py
+++ pet_memorial/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
-from pet_profile.models import PetOwner, PetPhoto
+from pet_profile.models import Pet, PetOwner, PetPhoto
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+import datetime
+import PIL.Image as Image
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -10,3 +14,14 @@ class HomeView(TemplateView):
         context['pet_photos'] = PetPhoto.objects.all()
         return context
     
+def pet_gallery(request):
+    pet_photos = Pet.pet_photos
+    return render(pet_photos)   #use render() or HttpResponse? not both?
+                                #how to render an image as the view(?)?
+
+
+### example
+#def current_datetime(request):
+#    now = datetime.datetime.now()
+#    html = "<html><body>It is now %s.</body></html>" % now
+#    return HttpResponse(html)
--- pet_profile/urls.py
+++ pet_profile/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
+
 from pet_profile import views
 
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     path("pet/photoupload/", views.PetPhotoUploadView.as_view(), name = "photo_upload"),
     path("owner/<slug:slug>/", views.PetOwnerDetailView.as_view(), name = "owner_profile"),
     path("pet/<slug:slug>/", views.PetDetailView.as_view(), name = "pet_profile"),
+    path("pet/<slug:slug>/gallery", views.pet_gallery),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #media does show--good!
