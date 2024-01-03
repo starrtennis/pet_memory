a09ea9b0a142437e15ffe644e562280b12ce9bc2
mostarrcoding <mostarrcoding@gmail.com>
2024-01-03 13:11:41 -0800

    accidentally deleted some stuff and violating dry

--- petGallery/views.py
+++ petGallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+class PetGalleryView(TemplateView):
+    template_name = "pet_gallery.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pet_owners'] = PetOwner.objects.all()
+        context['pet_photos'] = PetPhoto.objects.all()
+        return context
