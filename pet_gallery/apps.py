792e0dbdf9f976067300f0f0586d8df1f7fe68b0
mostarrcoding <mostarrcoding@gmail.com>
2024-03-08 19:10:14 -0800

    init'ed gallery app; initial concepts

---
+++ pet_gallery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PetGalleryConfig(AppConfig):
+    name = 'pet_gallery'
---
+++ pet_gallery/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# models:
+# node, pointer, and the horse wheel
+
+# an image of a pet and an image of a pet owner
+# is a bidirectional node
+class Node():
+    pass
+    
+# a pointer is the relationship between a pet and and
+# pet owner
+class Pointer():
+    pass
+
+# the horse_wheel class exists to control the party
+class horse_wheel():
+    pass
